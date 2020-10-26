13394e1010e4a461ee25e5d0eadc365468177793
Leon Prouger <leonprou@gmail.com>
2020-10-26 13:18:04 +0200

    adding allocation script

--- scripts/abi.sh
+++ scripts/abi.sh
@@ -7,3 +7,4 @@ fi
 mkdir abis
 
 ./node_modules/node-jq/bin/jq '.abi' build/contracts/VestingVault12.json > abis/VestingVault12.abi.json
+./node_modules/node-jq/bin/jq '.abi' build/contracts/WETH9.json > abis/WETH9.abi.json
