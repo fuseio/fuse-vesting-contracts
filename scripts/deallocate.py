efa5be4b7a933d34d5be22c85b8255134e10c746
Leon Prouger <leonprou@gmail.com>
2020-10-26 15:28:45 +0200

    adding dealocating

--- scripts/deallocate.py
+++ scripts/deallocate.py
@@ -8,9 +8,6 @@ import os
 import itertools
 load_dotenv()
 
-
-# VESTING_ADDRESS = '0xd2D13E4b2dD08C9E955d9DA9Ee8545254f499Af9'
-# VESTING_ADDRESS = '0xCc271d7379384EeCf10B7c43a8C675083853e5a4'
 VESTING_ADDRESS = os.environ.get('VESTING_ADDRESS')
 OWNER_ADDRESS = os.environ.get('OWNER_ADDRESS')
 
@@ -25,59 +22,60 @@ vestingAbi = json.load(abiFile)
 vestingContract = web3Fuse.eth.contract(abi=vestingAbi, address=VESTING_ADDRESS)
 
 
-def validateGrant(grant):
-  address = grant[1]
-  name = grant[0]
-  if not Web3.isAddress(address):
-    print('no valid address for {}, skipping'.format(name))
-    return False
-  if len(web3Ethereum.eth.getCode(address)) > 0:
-    print('address {} for {} is a contract, skipping'.format(address, name))
-    return False
+def getGrandId(grant):
+  address = grant[2]
+  name = grant[3]
+  print(grant)
   activeGrants = vestingContract.functions.getActiveGrants(address).call()
-  if len(activeGrants) > 0:
-    print('address {} for {} is already got a grant, skipping'.format(address, name))
-    return False
-
+  print(activeGrants)
+  # if len(activeGrants) > 1:
+  #   print('address {} for {} is already got a grant, skipping'.format(address, name))
+  #   return False
+  if len(activeGrants) == 0:
+    raise Exception('no grants for {}'.format(address))
   # print(len(web3Ethereum.eth.getCode(address)))
-  return True
+  return activeGrants[0]
 
 def findDups(grants):
   groups = print(list(itertools.groupby(grants, key=lambda g: g[1])))
   for g in groups:
     print(g[0])
 
-def allocateGrant(grant):
+def removeGrant(grant):
   global senderNonce
-  name = grant[0]
-  recipient = grant[1]
-  amount = web3Fuse.toWei(float(grant[2].replace(',', '')), 'ether')
-  startTime = int(grant[8])
-  duration = int(grant[9])
-  cliff = 0
-  print('allocating grant for {} with arguments - recipient: {}, start time: {}, amount: {}, duration in days: {}, cliff in days: {}'.format(name, recipient, startTime, amount, duration, cliff))
-  g = vestingContract.functions.addTokenGrant(recipient, startTime, amount, duration, cliff).buildTransaction({
+  name = grant[3]
+  recipient = grant[2]
+  grantId = getGrandId(grant)
+  # if not grantId or grantId != 0:
+  #   raise Exception('bad grant')
+  # vested = vestingContract.functions.getActiveGrants(address).call()
+  vested = vestingContract.functions.calculateGrantClaim(grantId).call()[1]
+  stats = vestingContract.functions.tokenGrants(grantId).call()
+  total = stats[1]
+  totalClaimed = stats[5]
+  locked = total - totalClaimed - vested
+  print('grant of {} for recepient {} with id {}. vested: {}, locked: {}, total: {}, totalClaimed: {}'.format(name, recipient, grantId, vested, locked, total, totalClaimed))
+  g = vestingContract.functions.removeTokenGrant(grantId).buildTransaction({
     'chainId': 122,
     'gasPrice': web3Fuse.toWei('1', 'gwei'),
     'from': '0xD418c5d0c4a3D87a6c555B7aA41f13EF87485Ec6',
     'nonce': senderNonce
   })
-  # web3Fuse.eth.sendTransaction({'to': '0xd3CdA913deB6f67967B99D67aCDFa1712C293601', 'from': '0xD418c5d0c4a3D87a6c555B7aA41f13EF87485Ec6', 'value': 1})
   private_key = os.environ.get('PRIVATE_KEY')
   signed_txn = web3Fuse.eth.account.signTransaction(g, private_key=private_key)
   txhash = web3Fuse.eth.sendRawTransaction(signed_txn.rawTransaction)
   tx_receipt = web3Fuse.eth.waitForTransactionReceipt(txhash)
+  print('executed in tx {}', txhash)
   senderNonce += 1
 
 def main():
   with open(sys.argv[1], 'r') as f:
     f.readline()
     reader = csv.reader(f)
-    data = list(reader)
+    grantsData = list(reader)
     # findDups(data)
-    validatedGrants = list(filter(validateGrant, data))
-    for grant in validatedGrants:
-      allocateGrant(grant)
+    for grant in grantsData:
+      removeGrant(grant)
 
 if __name__ == '__main__':
   main()
